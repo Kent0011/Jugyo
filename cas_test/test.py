ac1c56ae0b1f62d275dbffce774689f9d4b2454e
Kento Yamamoto <key7135g@gmail.com>
2025-11-18 12:13:19 +0900

    前画素との相互相関(途中)

--- cas_test/test.py
+++ cas_test/test.py
@@ -2,18 +2,21 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from cas_gui.base import CAS_GUI
 from cas_gui.threads.image_processor_class import ImageProcessorClass
-from scipy.ndimage import gaussian_filter
 import numpy as np
 
 # 処理クラスの定義
 class Filter(ImageProcessorClass):
     last_frame = None
     def process(self, inputFrame):
-        inputFrame = inputFrame.astype(np.float32)
-        f = np.fft.fft2(inputFrame)
-        f_inv = np.fft.ifft2(f)
-        psd2D_norm = np.abs(f_inv)
-        return psd2D_norm.astype(np.uint8)  
+        a = inputFrame.astype(np.float32)
+        if self.last_frame is None:
+            self.last_frame = a
+        b = self.last_frame.astype(np.float32)
+
+        result = np.fft.fft2(a) * np.fft.fft2(np.conj(b))
+        result = np.fft.ifft2(result)
+        self.last_frame = a
+        return np.abs(result)
 
 # GUIクラスの定義
 class ExampleGUI(CAS_GUI):
